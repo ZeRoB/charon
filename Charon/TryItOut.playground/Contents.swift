e3ffe0ab5d76b26c93700bb3c7bc04e8baf2489e
Robert <robert.zelhofer@gmail.com>
2020-03-22 21:55:08 +0100

    Links can be searched in Playground

--- Charon/TryItOut.playground/Contents.swift
+++ Charon/TryItOut.playground/Contents.swift
@@ -1,29 +1,48 @@
 import Foundation
 
-let home = FileManager.default.homeDirectoryForCurrentUser
-
-let playgroundPath = "Documents/Work/Freeletics/fl-application-ios"
-let playgroundUrl = home.appendingPathComponent(playgroundPath)
-
 enum SwiftType: String {
-    case classType = "class", structType = "struct", enumType = "enum", protocolType = "protocol"
+    case classType = "class", structType = "struct", enumType = "enum", protocolType = "protocol", extensionType = "extension"
 }
 
-func mainTypes(inFile url: URL) -> [(SwiftType, String)]? {
+func mainTypes(inFile url: URL) -> (mainType: [String], links: [(String, String)])? {
     do {
+        var stack = [String]()
+        var currentType: String?
         let file = try String(contentsOf: url, encoding: .utf8)
         let strings = file.components(separatedBy: .whitespacesAndNewlines)
-        var mainTypes = [(SwiftType, String)]()
+        var mainTypes = [String]()
+        var links = [(String, String)]()
         for (index, string) in strings.enumerated() {
-            if let swiftType = SwiftType(rawValue: String(string)),
+            if let _ = SwiftType(rawValue: String(string)),
                 let firstCharacter = strings[index + 1].first,
                 firstCharacter.isUppercase {
-                mainTypes.append((swiftType, String(strings[index + 1 ])))
+                let foundType = String(strings[index + 1 ])
+                mainTypes.append(foundType)
+                currentType = foundType
+//                print(string)
+                continue
+            }
+            if string == "{" {
+//                guard let currentType = currentType else {
+//                    assertionFailure("currentType cannot be nil if an { occurs. In \(url.absoluteString)")
+//                    continue
+//                }
+                stack.append(currentType ?? "")
+            }
+            if string == "}" {
+                currentType = stack.popLast()
+            }
+            if let currentType = currentType,
+                let firstCharacter = string.first,
+                firstCharacter.isUppercase {
+                links.append((currentType, string))
             }
         }
+        
+//        print(links)
         //        print("------")
         //        print(array)
-        return mainTypes
+        return (mainTypes, links)
     } catch {
         print(error)
         return nil
@@ -34,27 +53,44 @@ func mainTypes(inFile url: URL) -> [(SwiftType, String)]? {
 let fileManager = FileManager.default
 
 let resourceKeys : [URLResourceKey] = [.creationDateKey, .isDirectoryKey]
+let home = FileManager.default.homeDirectoryForCurrentUser
+
+let playgroundPath = "Documents/Work/Freeletics/fl-application-ios"
+let playgroundUrl = home.appendingPathComponent(playgroundPath)
 //    let documentsURL = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-let enumerator = FileManager.default.enumerator(at: playgroundUrl,
-                                                includingPropertiesForKeys: resourceKeys,
-                                                options: [.skipsHiddenFiles], errorHandler: { (url, error) -> Bool in
-                                                    print("directoryEnumerator error at \(url): ", error)
-                                                    return true
+let enumerator = FileManager.default.enumerator(
+    at: playgroundUrl,
+    includingPropertiesForKeys: resourceKeys,
+    options: [.skipsHiddenFiles],
+    errorHandler: { (url, error) -> Bool in
+        print("directoryEnumerator error at \(url): ", error)
+        return true
 })!
 
-var types = [String]()
+var maintypes = [String]()
+var links = [(String, String)]()
 
 for case let fileURL as URL in enumerator {
     // add Test for "h", "m"
     if ["swift"].contains(fileURL.pathExtension) {
-        if let mainTypes = mainTypes(inFile: fileURL), !mainTypes.isEmpty {
-            types.append(contentsOf: mainTypes.map({ $0.1 }))
-            print(fileURL.path)
-            print(mainTypes)
-            print()
+        if let mainTypesAndLinks = mainTypes(inFile: fileURL),
+            !mainTypesAndLinks.mainType.isEmpty {
+            maintypes.append(contentsOf: mainTypesAndLinks.mainType)
+            links.append(contentsOf: mainTypesAndLinks.links)
+//            print(fileURL.path)
+//            print(mainTypes)
+//            print()
         }
     }
 }
 
-print(types)
-print(types.count)
+print(links)
+print("MainTypes: \(maintypes.count) Links: \(links.count)")
+
+//let exampleFile = "/Users/rob/Documents/Work/Freeletics/fl-application-ios/Freeletics/Spaces/TrainingSpots/TrainingSpotDetailViewController.swift"
+//
+//let url = URL(fileURLWithPath: exampleFile)
+//let mainTypesAndLinks = mainTypes(inFile: url)
+//mainTypesAndLinks?.mainType
+//mainTypesAndLinks?.links
+
